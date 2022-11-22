55cfb8be96377f21f1cee7158e89ff3096a2de73
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-22 16:22:42 +0000

    set up signup for host + user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -15,3 +15,15 @@ router.post("/signupHost", async (req, res) => {
     console.log(error);
   }
 });
+
+router.post("/signupUser", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const salt = genSaltSync(10);
+    const hashedPassword = hashSync(password, salt);
+    await User.create({ email, hashedPassword });
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    console.log(error);
+  }
+});
