66af7a441462a9110e4868df454a3422c1b38ba4
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-24 10:09:36 +0100

    Added creation , updating and deletion of listings routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -14,4 +14,62 @@ router.get("/listings", async (req, res, next) => {
   }
 });
 
+router.get("/listings/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const listing = await Housing.findById(id);
+    res.json(listing);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/listings", async (req, res, next) => {
+  try {
+    const { country, city, typeOfRoom, placesAvailable, image } = req.body;
+    const newListing = await Housing.create({
+      country,
+      city,
+      typeOfRoom,
+      placesAvailable,
+      image,
+    });
+    res.json(newListing);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/listings/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { country, city, typeOfRoom, placesAvailable, image } = req.body;
+    const updatedListing = await Housing.findByIdAndUpdate(
+      id,
+      {
+        country,
+        city,
+        typeOfRoom,
+        placesAvailable,
+        image,
+      },
+      { new: true }
+    );
+    res.json(updatedListing);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/listings/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedListing = await Housing.findByIdAndDelete(id);
+    res.json(deletedListing);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 module.exports = router;
